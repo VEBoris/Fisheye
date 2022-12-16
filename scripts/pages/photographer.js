2d966b5d88780c1a62851be7d0804785739c3cac
VEBoris <Visuya@outlook.fr>
2022-12-16 02:07:11 +0100

    update dropdown filter

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,56 +3,59 @@ const prev = document.querySelector('.controls-left');
 let medias;
 let currentMedias = [];
 let filterItem = document.querySelectorAll('.filter-item');
+document.querySelector('.selected').innerHTML = 'Popularité';
 const mediaSection = document.querySelector('.photograph-body');
-
 
 for(let item of filterItem){
     item.addEventListener('click', filterAction, false);
 }
 function filterAction(e){
     let name = e.target.innerText;
+    filterItem.forEach(filter => {
+        filter.style.display = 'block';
+    });
+    e.target.style.display = 'none';
     filterData(name, currentMedias);
 }
 
 function filterData(type, medias){
-    const selectedFilter = document.querySelector('.selected');
+    const selectedFilter = document.querySelector('.selected').innerHTML = type;
     switch (type){
-        case 'Popularité':
-            medias.sort((a, b) => {
-                return b.likes - a.likes;
-            });
-            break;
-        case 'Date':
-            medias.sort((a, b) => {
-                return new Date(b.date) - new Date(a.date);
-            });
-            break;
-        case 'Titre':
-            medias.sort((a, b) => {
-                return a.title.localeCompare(b.title);
-            });
-            break;
-        default:
-            return medias;
-        }  
-        mediaSection.innerHTML = '';
-        medias.forEach(media => displayData(media, mediaSection));
+    case 'Popularité':
+        
+        medias.sort((a, b) => {
+            return b.likes - a.likes;
+        });
+        break;
+    case 'Date':
+        medias.sort((a, b) => {
+            return new Date(b.date) - new Date(a.date);
+        });
+        break;
+    case 'Titre':
+        medias.sort((a, b) => {
+            return a.title.localeCompare(b.title);
+        });
+        break;
+    default:
+        return medias;
+    }  
+    mediaSection.innerHTML = '';
+    medias.forEach(media => displayData(media, mediaSection));
+}
+function displayData(media, section){
+    const mediaModel = mediasFactory(media);
+    let mediaCardDOM;
+    if(Object.prototype.hasOwnProperty.call(media, 'image')){
+        mediaCardDOM = mediaModel.imgTemplate();
+    }else if(Object.prototype.hasOwnProperty.call(media, 'video')){
+        mediaCardDOM = mediaModel.videoTemplate(); 
     }
-    function displayData(media, section){
-        const mediaModel = mediasFactory(media);
-        let mediaCardDOM;
-        if(media.hasOwnProperty('image')){
-            //new AdaptedFilter(media);
-            mediaCardDOM = mediaModel.imgTemplate();
-        }else if(media.hasOwnProperty('video')){
-            //new AdaptedFilter(media);
-            mediaCardDOM = mediaModel.videoTemplate(); 
-        }
-          mediaSection.append(mediaCardDOM);
+    section.append(mediaCardDOM);
 }
 
-prev.addEventListener('click', prevImage)
-next.addEventListener('click', nextImage)
+prev.addEventListener('click', prevImage);
+next.addEventListener('click', nextImage);
 
 
 function prevImage(){
@@ -62,9 +65,9 @@ function prevImage(){
     if(index > 0){
         let currentMedia = currentMedias[index - 1];
         const lightbox = new Lightbox(currentMedia);
-        if (currentMedia.hasOwnProperty('image')){
+        if (Object.prototype.hasOwnProperty.call(currentMedia, 'image')){
             lightbox.createImgLightbox();
-        }else if(currentMedia.hasOwnProperty('video')){
+        }else if(Object.prototype.hasOwnProperty.call(currentMedia, 'video')){
             lightbox.createVidLightbox();
         }
     }
@@ -76,76 +79,79 @@ function nextImage(){
     if(index < currentMedias.length){
         let currentMedia = currentMedias[index + 1];
         const lightbox = new Lightbox(currentMedia);
-        if (currentMedia.hasOwnProperty('image')){
+        if (Object.prototype.hasOwnProperty.call(currentMedia, 'image')){
             lightbox.createImgLightbox();
-        }else if(currentMedia.hasOwnProperty('video')){
+        }else if(Object.prototype.hasOwnProperty.call(currentMedia, 'video')){
             lightbox.createVidLightbox();
         }
     }
 }
 
-//Mettre le code JavaScript lié à la page photographer.html
 async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
-    fetch("./data/photographers.json")
-    .then(function(res){
-        if(res.ok){
-            return res.json();
-        }
-    })
-    .then(function(data){
-        const photographers = data.photographers;
-        const photographersSection = document.querySelector(".photograph-header");
-        const Params = (new URL(document.location).searchParams);
-        const Id = Number(Params.get("id"));        
-        photographers.forEach((photographer) => {
-            if (photographer.id === Id) {
-                const photographerModel = photographerFactory(photographer);
-                const userCardDOM = photographerModel.user();
-                photographersSection.append(userCardDOM);   
+    fetch('./data/photographers.json')
+        .then(function(res){
+            if(res.ok){
+                return res.json();
             }
-        });
-    })  
-    .catch(function(err){
-    })
-    // et bien retourner le tableau photographers seulement une fois
+        })
+        .then(function(data){
+            const photographers = data.photographers;
+            const photographersSection = document.querySelector('.photograph-header');
+            const Params = (new URL(document.location).searchParams);
+            const Id = Number(Params.get('id'));
+            photographers.forEach((photographer) => {
+                if (photographer.id === Id) {
+                    const photographerModel = photographerFactory(photographer);
+                    const userCardDOM = photographerModel.user();
+                    photographersSection.append(userCardDOM);   
+                }
+            });
+        })
 }
 
 async function getMedias(){
     fetch('./data/photographers.json')
-    .then(function(res){
-        if(res.ok){
-            return res.json()
-        }
-    })
-    .then(function(data){
-        medias = data.media;
-        const Params = (new URL(document.location).searchParams);
-        const Id = Number(Params.get("id"));
+        .then(function(res){
+            if(res.ok){
+                return res.json();
+            }
+        })
+        .then(function(data){
+            //
+            medias = data.media;
+            const Params = (new URL(document.location).searchParams);
+            const Id = Number(Params.get('id'));
 
-        let sum = 0;
-
-        medias.forEach((media) => {
-            if(media.photographerId === Id){
-                currentMedias.push(media);
-                sum += media.likes;
-                const likesBlock = document.querySelector(".photograph-likes");
-                likesBlock.textContent = sum;
-                function nextLightbox(){
-                }
-                next.addEventListener("click", nextLightbox);
-                const icon = document.createElement('i');
-                icon.classList.add('tot-likes-icon' ,'fa-solid', 'fa-heart');
-                likesBlock.append(icon)
-                displayData(media, mediaSection);
-            }   
+            // sommes des likes
+            let sum = 0;
+            medias.forEach((media) => {
+                    // verification d'une correspondence des medias avec l'ID des photographes
+                
+                    if(media.photographerId === Id){
+                        // somme des likes au total
+                        currentMedias.push(media);
+                        sum += media.likes;
+                        const likesBlock = document.querySelector('.photograph-likes');
+                        likesBlock.textContent = sum;
+                        
+                        const icon = document.createElement('i');
+                        icon.classList.add('tot-likes-icon' ,'fa-solid', 'fa-heart');
+                        likesBlock.append(icon);
+                        // vérication image/vidéo
+                        displayData(media, mediaSection);
+                    }   
+                });
+        })
+        .catch(function(err){
+            console.log(err);
         });
-    })
 }
+
 
 async function init() {
     // Récupère les datas des photographes
     await getPhotographers();
     await getMedias();
-};
+}
 init();
